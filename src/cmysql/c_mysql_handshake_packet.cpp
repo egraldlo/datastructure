bf23f980a8a29ce77709d6c18f981f85d32f8a19
casa <casa@180.168.41.175>
2014-05-27 00:38:46 +0800

    序列化报文

--- src/cmysql/c_mysql_handshake_packet.cpp
+++ src/cmysql/c_mysql_handshake_packet.cpp
@@ -8,7 +8,18 @@
 #include "c_mysql_handshake_packet.h"
 
 CMysqlHandshakePacket::CMysqlHandshakePacket() {
-
+    const char* str = "5.5.1";
+    protocol_version_ = 10;//Protocol::HandshakeV10
+    string server_version(str);
+    server_version_=server_version;
+    thread_id_=100;//
+    memset(scramble_buff_,'a',9);
+    server_capabilities_=63487;
+    server_language_=8;//latin1_swedish_ci
+    server_status_=0;// no this value in mysql protocol document
+    memset(plugin_,0,sizeof(plugin_));
+    memset(plugin2_,'b',12);
+    terminated_='\0';
 }
 
 CMysqlHandshakePacket::~CMysqlHandshakePacket() {
@@ -28,13 +39,19 @@ int CMysqlHandshakePacket::serialize(char *buffer,int len,long int &pos) {
 	 *server_capabilities_: 服务器权能标志                   2
 	 *server_language_: 字符编码                           1
 	 *server_status_: 服务器状态                            2
-     *char plugin_[13]: "ob中服务器权能，挑战长度和填充值的和"  13
-     *char plugin2_[12]: 第二波挑战随机数，至少12字节         12
+    *char plugin_[13]: "ob中服务器权能，挑战长度和填充值的和"  13
+    *char plugin2_[12]: 第二波挑战随机数，至少12字节         12
 	 *terminated_: 结束                                   1
 	 *------------------报文头-----------------
 	 *下面是要放到报文头中
 	 *(header size)pkt_len: 消息长度				          3
 	 *(header seq)0： 消息序号                              1
 	 */
+	string packet;
 	cout<<"开始序列化报文体和报文头！"<<endl;
+	//三个字节的 length=48;
+	//一个字节的 seq=0;
+
+
+
 }
--- src/cmysql/c_mysql_handshake_packet.h
+++ src/cmysql/c_mysql_handshake_packet.h
@@ -8,6 +8,7 @@
 #ifndef C_MYSQL_HANDSHAKE_PACKET_H_
 #define C_MYSQL_HANDSHAKE_PACKET_H_
 
+#include <string>
 #include <iostream>
 using namespace std;
 
@@ -21,7 +22,20 @@ public:
 	int serialize(char *buffer,int len,long int &pos);
 
 private:
-	int thread_id_;//connection id
+    uint8_t protocol_version_;
+    string server_version_;
+    int thread_id_;//connection id
+    char scramble_buff_[8];// auth_plugin_data_part_1 : first 8 bytes of the auth-plugin data
+    int server_capabilities_;  /* set value to use 4.1protocol */
+    int server_language_;
+    int server_status_;
+    char plugin_[13];        /* always 0x00 */
+    char plugin2_[12];
+    char terminated_;
+    string str_buf_;   //store ObString content
+
+
+
 };
 
 #endif /* CMYSQLHANDSHAKEPACKET_H_ */
