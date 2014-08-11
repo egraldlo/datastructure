83ed4f9f0bf7423ba672932da7662be28d4bf743
egraldlo <egraldlo@gmail.com>
2014-08-11 19:58:03 +0800

    some arrays.

--- src/leetcode/ArrayAlgos.h
+++ src/leetcode/ArrayAlgos.h
@@ -16,39 +16,9 @@ public:
 	ArrayAlgos();
 	virtual ~ArrayAlgos();
 
-	void merge(int A[],int m,int B[],int n){
-		for(int i=m;i>0;i--){
-			A[i+n-1]=A[i-1];
-			cout<<"A[i+n-1]=A[i]: "<<A[i+n-1]<<"="<<A[i]<<endl;
-		}
-		int k=0,l=0,h=0;
-		while(k<m&&l<n){
-			if(A[k+n]<B[l]){
-				A[h]=A[k+n];
-				cout<<"h: "<<k+n<<": "<<A[h]<<endl;
-				h++;
-				k++;
-			}
-			else{
-				A[h]=B[l];
-				h++;
-				l++;
-			}
-		}
-		while(k<m){
-			A[h]=A[k+n];
-			h++;
-			k++;
-		}
-		while(l<n){
-			A[h]=B[l];
-			h++;
-			l++;
-		}
-		for(int i=0;i<2;i++){
-			cout<<A[i]<<endl;
-		}
-	}
+public:
+	void msort();
+	void merge(int A[],int m,int B[],int n);
 };
 
 #endif /* ARRAYALGOS_H_ */
